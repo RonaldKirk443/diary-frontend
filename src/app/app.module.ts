04f12aabe381a31f1507d770e4a9ca0ab3056df6
RonaldKirk443 <ronaldkirk101pc@gmail.com>
2023-07-08 19:25:22 -0400

    Error testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http"
 
 import { AppComponent } from './app.component';
 import { UiModule } from "./ui/ui.module";
@@ -12,7 +13,8 @@ import { PagesModule} from "./pages/pages.module";
   imports: [
     BrowserModule,
     UiModule,
-    PagesModule
+    PagesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
