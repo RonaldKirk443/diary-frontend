1d3778ba94972a4293044caa60642e4d52aaa732
RonaldKirk443 <ronaldkirk101pc@gmail.com>
2023-07-26 17:11:30 -0400

    FIxed popper basics
    Popper now works as it should but it now needs a little styling and preferably exported to a separate class because right now its duplicated code.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm} from '@angular/forms'
 import {Login} from "../../models/login";
 import {AuthService} from "../../auth/services/auth.service";
-
+import {createPopper, Instance} from "@popperjs/core";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -13,8 +13,70 @@ export class LoginComponent {
     email: "",
     pass: ""
   }
+  popupErrorMsg : String = "You shouldn't be seeing this";
 
   constructor(private authService : AuthService) {
+  }
+
+  ngOnInit() {
+    const emailErrorIcon = document.querySelector<HTMLElement>('#emailErrorIcon');
+    const passErrorIcon = document.querySelector<HTMLElement>('#passErrorIcon');
+    const popup = document.querySelector<HTMLElement>('#errorPopup');
+    if (!emailErrorIcon || !passErrorIcon || !popup) return;
+    this.initErrorPopup(emailErrorIcon, popup, "Email cannot be empty");
+    this.initErrorPopup(passErrorIcon, popup, "Pass cannot be empty");
+  }
+
+  initErrorPopup(obj : HTMLElement, popup : HTMLElement, errorMsg : String) {
+    let popperInstance : Instance;
+    const showEvents = ['mouseenter'];
+    const hideEvents = ['mouseleave'];
+
+    popperInstance = createPopper(obj, popup, {
+      placement: 'right'
+    });
+
+    showEvents.forEach((event) => {
+      obj.addEventListener(event, () => {
+        this.popupErrorMsg = errorMsg;
+        showErrorPopup();
+      });
+    });
+    hideEvents.forEach((event) => {
+      obj.addEventListener(event, () => {
+        hide();
+      });
+    });
+
+    function showErrorPopup() {
+      popup.setAttribute('data-show', '');
+
+      popperInstance.setOptions((options) => ({
+        ...options,
+        modifiers: [
+          {
+            name: 'offset',
+            options: {
+              offset: [0, 7],
+            },
+          },
+          { name: 'eventListeners', enabled: true },
+        ],
+      }));
+
+      popperInstance.update();
+    }
+
+    function hide() {
+      popup.removeAttribute('data-show');
+
+      popperInstance.setOptions((options) => ({
+        ...options,
+        modifiers: [
+          { name: 'eventListeners', enabled: false },
+        ],
+      }));
+    }
   }
 
   loginUser() : void {
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm} from '@angular/forms'
 import { RegisterService} from "./register.service";
 import {User} from "../../models/user";
+import {createPopper, Instance} from "@popperjs/core";
 
 @Component({
   selector: 'app-register',
@@ -9,15 +10,81 @@ import {User} from "../../models/user";
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  user: User = {
-    id: 0,
-    username: '',
-    email: '',
-    birthday: new Date("2010-3-25")
-  };
+  popupErrorMsg : String = "You should not be seeing this";
+  user: User = new User();
 
   constructor(private registerService: RegisterService) {
   }
+
+  ngOnInit() {
+    this.user.birthday = new Date("2010-3-25");
+    const usernameErrorIcon = document.querySelector<HTMLElement>('#usernameErrorIcon');
+    const emailErrorIcon = document.querySelector<HTMLElement>('#emailErrorIcon');
+    const passErrorIcon = document.querySelector<HTMLElement>('#passErrorIcon');
+    const cPassErrorIcon = document.querySelector<HTMLElement>('#cPassErrorIcon');
+    const birthdayErrorIcon = document.querySelector<HTMLElement>('#birthdayErrorIcon');
+    const popup = document.querySelector<HTMLElement>('#errorPopup');
+
+    if (!usernameErrorIcon || !emailErrorIcon || !passErrorIcon || !cPassErrorIcon || !birthdayErrorIcon || !popup) return;
+
+    this.initErrorPopup(usernameErrorIcon, popup, "Username cannot be empty");
+    this.initErrorPopup(emailErrorIcon, popup, "Email cannot be empty");
+    this.initErrorPopup(passErrorIcon, popup, "Pass cannot be empty");
+    this.initErrorPopup(cPassErrorIcon, popup, "Passwords do not match");
+    this.initErrorPopup(birthdayErrorIcon, popup, "Date is invalid")
+  }
+
+  initErrorPopup(obj : HTMLElement, popup : HTMLElement, errorMsg : String) {
+    let popperInstance : Instance;
+    const showEvents = ['mouseenter'];
+    const hideEvents = ['mouseleave'];
+
+    popperInstance = createPopper(obj, popup, {
+      placement: 'right'
+    });
+
+    showEvents.forEach((event) => {
+      obj.addEventListener(event, () => {
+        this.popupErrorMsg = errorMsg;
+        showErrorPopup();
+      });
+    });
+    hideEvents.forEach((event) => {
+      obj.addEventListener(event, () => {
+        hide();
+      });
+    });
+
+    function showErrorPopup() {
+      popup.setAttribute('data-show', '');
+
+      popperInstance.setOptions((options) => ({
+        ...options,
+        modifiers: [
+          {
+            name: 'offset',
+            options: {
+              offset: [0, 7],
+            },
+          },
+          { name: 'eventListeners', enabled: true },
+        ],
+      }));
+
+      popperInstance.update();
+    }
+
+    function hide() {
+      popup.removeAttribute('data-show');
+      popperInstance.setOptions((options) => ({
+        ...options,
+        modifiers: [
+          { name: 'eventListeners', enabled: false },
+        ],
+      }));
+    }
+  }
+
   registerUser() : void {
     this.registerService.addUser(this.user).subscribe();
   }
