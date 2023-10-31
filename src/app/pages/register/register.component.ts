9372cb7c5ab07a284ce3977de2088b743da6f2c9
LubyLub <seyed8erfan8@gmail.com>
2023-10-30 20:06:50 -0400

    calendar done

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,29 +1,56 @@
 import { Component } from '@angular/core';
-import {NgForm, NgModel} from '@angular/forms'
+import {FormControl, NgForm, NgModel} from '@angular/forms'
 import {User} from "../../models/user";
 import {popupErrorMsg, ValidationErrorPopupService} from "../../ui/services/validation-error-popup.service";
 import {AuthService} from "../../auth/services/auth.service";
 import {Login} from "../../models/login";
 import {Router} from "@angular/router";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {map, Observable} from "rxjs";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {default as _rollupMoment} from "moment/moment";
+import * as _moment from "moment/moment";
+
+
+const moment = _rollupMoment || _moment;
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+  },
+  display: {
+    dateInput: 'LL',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
+
 export class RegisterComponent {
   popupErrorMsg: popupErrorMsg;
   user: User = new User();
   login: Login = new Login();
   // cPassForm: NgModel;
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches));
+  date = new FormControl(moment());
+  maxDate: Date;
 
-  constructor(private authService: AuthService, private validationErrorPopupService: ValidationErrorPopupService, private router: Router, private snackBar: MatSnackBar) {
+  constructor(private authService: AuthService, private validationErrorPopupService: ValidationErrorPopupService, private router: Router, private snackBar: MatSnackBar, private breakpointObserver: BreakpointObserver) {
     this.popupErrorMsg = validationErrorPopupService.popupErrorMsg;
+    this.maxDate = new Date();
+    this.user.birthday = this.maxDate;
   }
 
   ngOnInit() {
-    this.user.birthday = new Date("2010-3-25");
     const usernameErrorIcon = document.querySelector<HTMLElement>('#usernameErrorIcon');
     const emailErrorIcon = document.querySelector<HTMLElement>('#emailErrorIcon');
     const passErrorIcon = document.querySelector<HTMLElement>('#passErrorIcon');
@@ -43,6 +70,7 @@ export class RegisterComponent {
   }
 
   registerUser() : void {
+    console.log(this.user);
     this.authService.addUser(this.user).subscribe({
       next: (user: User) => {
         this.login.userId = user.id;
